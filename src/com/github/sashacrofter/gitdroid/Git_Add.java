4ae1c7cc7449b9221184b86d8c7503e394b42e2a
Sasha Crofter <sasha@crofter.org>
2012-03-25 19:42:27 -0400

    improved Git_Add

--- src/com/github/sashacrofter/gitdroid/Git_Add.java
+++ src/com/github/sashacrofter/gitdroid/Git_Add.java
@@ -2,6 +2,9 @@ package com.github.sashacrofter.gitdroid;
 
 import java.util.HashMap;
 
+import org.eclipse.jgit.api.AddCommand;
+import org.eclipse.jgit.api.errors.NoFilepatternException;
+
 
 public class Git_Add extends GitBase {
 	
@@ -10,6 +13,13 @@ public class Git_Add extends GitBase {
 	 */
 	public static String run(AGit agit, String[] args, HashMap<String, String> argmap)
 	{
+		AddCommand add = agit.getGit().add();
+		try {
+			add.addFilepattern(args[2]).call(); //TODO add multiple file support
+		} catch (NoFilepatternException e) { //TODO never hit this catch
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		//TODO do JGit add call
 		return "Called Add";
 	}
