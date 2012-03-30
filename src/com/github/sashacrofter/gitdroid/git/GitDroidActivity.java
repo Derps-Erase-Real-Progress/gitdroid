4963943b635a0d2fd1450266ad72762191455e40
Sasha Crofter <sasha@crofter.org>
2012-03-30 09:59:19 -0400

    layout fix added TextView improved GitDroid Activity added debug logs

--- src/com/github/sashacrofter/gitdroid/git/GitDroidActivity.java
+++ src/com/github/sashacrofter/gitdroid/git/GitDroidActivity.java
@@ -22,15 +22,19 @@ public class GitDroidActivity extends Activity {
 	private EditText commandInput;
 	private TextView outputTextView;
 	
+	private final String tag = "GD";
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
-    	Log.d("gitdroidActivity", "called onCreate()");
+    	Log.d(tag, "called onCreate()");
     	this.session = new TerminalEmulator();
     	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Log.d(tag, "making View objects");
         
         goButton = (Button) findViewById(R.id.goButton);
         commandInput = (EditText) findViewById(R.id.commandInput);
@@ -39,6 +43,7 @@ public class GitDroidActivity extends Activity {
         goButton.setOnClickListener(
         		new View.OnClickListener() {
 					public void onClick(View v) {
+						Log.d(tag, "executing command");
 						session.execute(commandInput.getText().toString());
 					}
 				});
@@ -46,6 +51,6 @@ public class GitDroidActivity extends Activity {
     
     public void output(String string)
     {
-    	outputTextView.setText(outputTextView.getText()+"\n"+string);
+    	outputTextView.append("\n"+string);
     }
 }
